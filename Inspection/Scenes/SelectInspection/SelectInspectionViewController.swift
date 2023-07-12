4fc307ea3036111882e66aef5fa2d033598b37a3
chou8018 <jay8018_ios@126.com>
2023-07-12 20:55:08 +0800

    fixed location

--- Inspection/Scenes/SelectInspection/SelectInspectionViewController.swift
+++ Inspection/Scenes/SelectInspection/SelectInspectionViewController.swift
@@ -189,7 +189,7 @@ class SelectInspectionViewController: UIViewController, SelectInspectionDisplayL
     }
     
     @objc func showPlace(){
-        performSegue(withIdentifier: "showPlaceSegue", sender: nil)
+//        performSegue(withIdentifier: "showPlaceSegue", sender: nil)
     }
     
     @objc func showPickupCar(){
